71400a0a5f14e8ea5b65d17d020926c00d277d78
nandogebru <119262784+nandogebru@users.noreply.github.com>
2024-02-13 21:50:19 -0600

    Java class introduction personal study.

---
+++ Bicycle.java
@@ -0,0 +1,10 @@
+public class Bicycle {
+    //fields of class
+    int gear = 5;
+
+    //behavior or method
+    public void braking(){
+        System.out.println("working of Braking");
+    }
+
+}
---
+++ Main.java
@@ -0,0 +1,25 @@
+/** this program is an introduction to java opp, on classes and objects as well as members (feilds && methods)
+**/
+import java.util.*;
+public class Main {
+    public static void main(String[] args){
+
+        //creating sportsBicycle object
+        Bicycle sportsBicycle = new Bicycle();
+
+        //creating touringBicycle object
+        Bicycle touringBicycle = new Bicycle();
+
+
+        //access feilds and methods
+        int gear = sportsBicycle.gear;
+
+        //output it
+        System.out.println("Bike gear : " + gear);
+
+//        sportsBicycle.gear;
+        sportsBicycle.braking();
+
+
+    }
+}
